a29d0f83868a2e95afbe52a080b85f7aeea9bef0
Tsabit Haqqi <t.haqqi@student.saedubai.com>
2025-04-14 13:10:36 +0400

    Close

--- Assets/Scripts/TaskPanelManager.cs
+++ Assets/Scripts/TaskPanelManager.cs
@@ -5,11 +5,13 @@ public class TaskPanelManager : MonoBehaviour
 {
     [SerializeField] private GameObject taskPanel;
     [SerializeField] private Button taskButton;
+    [SerializeField] private Button panelCloseButton;
 
     private void Start()
     {
         taskPanel.SetActive(false);
         taskButton.onClick.AddListener(ToggleTaskPanel);
+        panelCloseButton.onClick.AddListener(ToggleTaskPanel);
     }
 
     public void ToggleTaskPanel()
